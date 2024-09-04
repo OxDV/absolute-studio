e621f7e01ca47290b4d8780596e55765d0c46642
0xDV <di593297724ma@gmail.com>
2024-09-04 22:23:48 +0200

    implemented page

---
+++ src/components/TextBlock.js
@@ -0,0 +1,47 @@
+'use client'
+
+function TextBlock() {
+	return (
+		<div id='textblock'>
+			<div id='textblock-container'>
+				<h1 id='textblock-title'>What is Absolute Web Studio?</h1>
+				<p id='textblock-content'>
+					I offer professional web development and web design services. From
+					simple one-page websites to complex e-commerce solutions – I bring
+					your ideas to life!
+					<br />
+					<br />
+					Services: <br />
+					<br />- Creation of websites of any complexity: From simple landing
+					pages to online stores
+					<br />
+					<br />- Short-term and long-term projects
+					<br />
+					<br />- Using the latest technologies: React, TypeScript, JavaScript,
+					CSS, HTML, Node.js
+					<br />
+					<br />- Web3 development with Solidity for blockchain projects
+					<br />
+					<br />- Design based on your drafts or custom-designed by me, if
+					desired
+					<br />
+					<br />- Optimization for PC, tablet, and smartphones, so your website
+					is perfectly displayed on all devices
+					<br />
+					<br />- Basic SEO setup to help you get found more easily on Google
+					<br />
+					<br />- Hosting and domain setup, all from one source, so you don’t
+					have to worry about anything
+				</p>
+			</div>
+			<footer id='textblock-footer'>
+				Created by Absolute Web Studio<br/><br/>
+				<a id='write-to-mail' href='mailto:dev.vyshnevskyi@gmail.com'>
+					Email
+				</a>
+			</footer>
+		</div>
+	)
+}
+
+export default TextBlock
